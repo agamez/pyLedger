96572e7919eae6f70077d8abef693b19c89dde04
Alvaro G. M <alvaro.gamez@hazent.com>
2014-08-02 16:10:21 +0200

    ledger.py: store all entries in one list, create a dictionary with entries by payer

--- ledger.py
+++ ledger.py
@@ -4,16 +4,18 @@ import os
 class Ledger():
 	def split_ledger_contents(self):
 		self.people = self.file_contents[1].upper().split()
-		self.entries = dict()
+		self.entries_by_payer = dict()
+		self.entries = list()
 		for entry in self.file_contents[2:]:
 			entry = entry.split()
 			entry[4] = " ".join(entry[4:])
 			entry = entry[:5]
 			persona = entry[3].upper() 
 
-			if persona not in self.entries:
-				self.entries[persona] = list()
-			self.entries[persona].append(entry)
+			if persona not in self.entries_by_payer:
+				self.entries_by_payer[persona] = list()
+			self.entries_by_payer[persona].append(entry)
+			self.entries.append(entry)
 
 	def __init__(self, file="~/.pyLedger/default.ldgr"):
 		self.file=os.path.expanduser(file)
