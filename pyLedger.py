8ab6fbfd30a7f0f2badd134af8a0bb0f8426bf7b
Alvaro G. M <alvaro.gamez@hazent.com>
2014-08-02 16:13:21 +0200

    pyLedger: show entries in a table

--- pyLedger.py
+++ pyLedger.py
@@ -5,14 +5,30 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import pyLedger_ui
 import editEntry_ui
+from ledger import Ledger
+
 
 class PyLedger(QMainWindow, pyLedger_ui.Ui_MainWindow):
-	def __init__(self):
+	def __init__(self, ledger):
 		super(PyLedger, self).__init__()
 		self.setupUi(self)
 
+		print ledger.entries
+		row=0
+		for entry in ledger.entries:
+			entryItems = map(QTableWidgetItem, entry)
+			self.entriesTable.setRowCount(self.entriesTable.rowCount()+1)
+			column=0
+			for item in entryItems:
+				self.entriesTable.setItem(row,column,item)
+				column+=1
+			print entry, "OK"
+			row+=1
+
+
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
-	form = PyLedger()
+	ledger = Ledger()
+	form = PyLedger(ledger)
 	form.show()
 	app.exec_()
