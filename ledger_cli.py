96d32c0b5c93ce99aa390bf56118404fa7dc4d79
Alvaro G. M <alvaro.gamez@hazent.com>
2014-11-10 17:23:27 +0100

    ledger_cli.py: move code to function  calculate_expenses_person

--- ledger_cli.py
+++ ledger_cli.py
@@ -4,12 +4,9 @@ import os
 def persona_total(persona):
 	return sum([float(x[2]) for x in persona]) 
 
-
-if __name__ == '__main__':
+def calculate_expenses_person(ledger_contents):
 	expenses = dict()
-	ledger_file = open(os.path.expanduser("~/.pyLedger/default.ldgr"), 'r').readlines()[2:]
-
-	for entry in ledger_file:
+	for entry in ledger_contents:
 		entry = entry.split()
 		entry[4] = " ".join(entry[4:])
 		entry = entry[:5]
@@ -18,7 +15,12 @@ if __name__ == '__main__':
 		if persona not in expenses:
 			expenses[persona] = list()
 		expenses[persona].append(entry)
+	return expenses
 
+if __name__ == '__main__':
+	ledger_contents = open(os.path.expanduser("~/.pyLedger/default.ldgr"), 'r').readlines()[2:]
+
+	expenses = calculate_expenses_person(ledger_contents)
 	for key in expenses:
 		print key,":",
 		expenses[key] = persona_total(expenses[key]) 
