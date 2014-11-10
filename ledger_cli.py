bfabae516431e15cc3c0685a9484b05892de2839
Alvaro G. M <alvaro.gamez@hazent.com>
2014-11-10 17:16:10 +0100

    ledger_cli.py: place code below __main__ if

--- ledger_cli.py
+++ ledger_cli.py
@@ -4,26 +4,28 @@ import os
 def persona_total(persona):
 	return sum([float(x[2]) for x in persona]) 
 
-expenses = dict()
-ledger_file = open(os.path.expanduser("~/.pyLedger/default.ldgr"), 'r').readlines()[2:]
 
-for entry in ledger_file:
-	entry = entry.split()
-	entry[4] = " ".join(entry[4:])
-	entry = entry[:5]
-	persona = entry[3].upper() 
+if __name__ == '__main__':
+	expenses = dict()
+	ledger_file = open(os.path.expanduser("~/.pyLedger/default.ldgr"), 'r').readlines()[2:]
 
-	if persona not in expenses:
-		expenses[persona] = list()
-	expenses[persona].append(entry)
+	for entry in ledger_file:
+		entry = entry.split()
+		entry[4] = " ".join(entry[4:])
+		entry = entry[:5]
+		persona = entry[3].upper() 
 
-for key in expenses:
-	print key,":",
-	expenses[key] = persona_total(expenses[key]) 
-	print expenses[key] 
-print
+		if persona not in expenses:
+			expenses[persona] = list()
+		expenses[persona].append(entry)
 
-total = sum([expenses[key] for key in expenses]) 
-print "TOTAL:", total
-for key in expenses:
-	print key, "owes:", total/len(expenses) - expenses[key]
+	for key in expenses:
+		print key,":",
+		expenses[key] = persona_total(expenses[key]) 
+		print expenses[key] 
+	print
+
+	total = sum([expenses[key] for key in expenses]) 
+	print "TOTAL:", total
+	for key in expenses:
+		print key, "owes:", total/len(expenses) - expenses[key]
