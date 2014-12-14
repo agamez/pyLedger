9e41ead2b552d8f5760761ed072ff3ea04423e92
Alvaro G. M <alvaro.gamez@hazent.com>
2014-12-14 17:32:17 +0100

    setup.py: distutils script for installation. pyLedger.desktop modified to point to installation path

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from distutils.command.build import build
+import os
+
+class build_ui(build):
+	def run(self):
+		build.run(self)
+		os.system("pyuic4 pyLedger.ui -o pyLedger_ui.py")		
+
+
+setup(
+	name='pyLedger',
+	version='1.0',
+	license = "GNU General Public License, Version 2",
+	author='Alvaro Gamez Machado',
+	author_email='alvaro.gamez@hazent.com',
+	url='https://github.com/agamez/pyLedger',
+	scripts=['ledger.py', 'pyLedger.py', 'pyLedger_ui.py'],
+	cmdclass = {"build" : build_ui},
+	data_files = [('/opt/usr/share/icons/hicolor/64x64/apps', ['ledger.png']),
+		      ('/usr/share/applications/hildon', ['pyLedger.desktop'])
+	]
+)
